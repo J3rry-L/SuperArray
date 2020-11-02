c0e36c0e741240587dd5fa8c59cf00751c98dfb2
J3rry-L <jliang21@stuy.edu>
2020-11-01 20:25:54 -0500

    Added constructor, toString methods and tested

--- SuperArray.java
+++ SuperArray.java
@@ -1,4 +1,14 @@
+import java.util.Arrays;
 public class SuperArray{
   private String [] data;
   private int size;
+
+  public SuperArray(){
+    data = new String[10];
+    size = 0;
+  }
+
+  public String toString(){
+    return(Arrays.toString(data) + "\t" + "Current Size = " + size);
+  }
 }
